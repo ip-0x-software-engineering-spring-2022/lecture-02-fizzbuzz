a258fdc4638bac57c3785c8b89b9f0b7c7906797
Oleksandr Kovalchuk <anxolerd@outlook.com>
2022-02-08 11:53:34 +0200

    Implement algorithm logic
    Implement fizzbuzz function to actually work.
    
    Ref: https://en.wikipedia.org/wiki/Fizz_buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,7 +5,14 @@ We do this just for demonstration
 
 
 def fizzbuzz(i):
-    pass
+    if i % 15 == 0:
+        print("fizzbuzz")
+    elif i % 5 == 0:
+        print("buzz")
+    elif i % 3 == 0:
+        print("fizz")
+    else:
+        print(str(i))
 
 for i in range(27):
     fizzbuzz(i)
