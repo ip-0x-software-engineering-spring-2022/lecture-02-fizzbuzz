8a2a3d2205840ae1899d873ce5e6c19b31155289
Oleksandr Kovalchuk <anxolerd@outlook.com>
2022-02-08 12:01:50 +0200

    Implement CLI interface
    Pass upper bound for fizbuzz algorithm from CLI

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,6 +3,8 @@ This is an implementation of famous fizzbuzz problem
 We do this just for demonstration
 """
 
+import argparse
+
 
 def fizzbuzz(i):
     if i % 15 == 0:
@@ -14,5 +16,10 @@ def fizzbuzz(i):
     else:
         print(str(i))
 
-for i in range(27):
-    fizzbuzz(i)
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('number', type=int)
+    cli = ap.parse_args()
+
+    for i in range(cli.number):
+        fizzbuzz(i)
